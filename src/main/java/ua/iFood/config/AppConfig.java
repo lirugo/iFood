2ae247625ca228640e3fd30070e0168c5adbeb8c
lirugo <roostik21@gmail.com>
2019-07-09 12:16:40 +0300

    Initial commit

---
+++ src/main/java/ua/iFood/config/AppConfig.java
@@ -0,0 +1,9 @@
+package ua.iFood.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ua.iFood.*")
+public class AppConfig {
+}
---
+++ src/main/java/ua/iFood/config/SpringInitializer.java
@@ -0,0 +1,27 @@
+package ua.iFood.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class SpringInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(AppConfig.class);
+        servletContext.addListener(new ContextLoaderListener(appContext));
+
+        // Dispatcher Servlet
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("SpringDispatcher",
+                new DispatcherServlet(appContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+
+}
