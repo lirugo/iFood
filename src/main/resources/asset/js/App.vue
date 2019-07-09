42f9315a86a11962091752a77d8c9c43ad31213c
lirugo <roostik21@gmail.com>
2019-07-09 14:46:16 +0300

    Added vue-router and simple mockup welcome page

--- src/main/resources/asset/js/App.vue
+++ src/main/resources/asset/js/App.vue
@@ -2,6 +2,12 @@
     <v-app>
         <v-container fluid>
             <Navigation/>
+            <transition
+                    name="fade"
+                    mode="out-in"
+            >
+                <router-view></router-view>
+            </transition>
         </v-container>
     </v-app>
 </template>
@@ -15,3 +21,17 @@
         }
     }
 </script>
+
+<style>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition-duration: 0.3s;
+        transition-property: opacity;
+        transition-timing-function: ease;
+    }
+
+    .fade-enter,
+    .fade-leave-active {
+        opacity: 0
+    }
+</style>
--- src/main/resources/asset/js/main.js
+++ src/main/resources/asset/js/main.js
@@ -6,7 +6,23 @@ import Vuetify from 'vuetify'
 Vue.use(Vuetify)
 import 'vuetify/dist/vuetify.min.css'
 
-new Vue({
-    el: '#app',
-    render: a => a(App)
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+Vue.config.productionTip = false
+
+import Welcome from './components/Welcome.vue'
+
+const routes = [
+    { path: '/welcome', component: Welcome },
+]
+
+const router = new VueRouter({
+    routes
 })
+
+
+new Vue({
+    render: h => h(App),
+    router,
+}).$mount('#app')
