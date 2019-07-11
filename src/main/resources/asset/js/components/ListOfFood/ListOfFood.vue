f550d3aae9cade14e752745f2c839a669499dacb
lirugo <roostik21@gmail.com>
2019-07-11 08:19:48 +0300

    Added ListOfEatenFood mock up

--- src/main/resources/asset/js/components/ListOfFood/ListOfFood.vue
+++ src/main/resources/asset/js/components/ListOfFood/ListOfFood.vue
@@ -1,95 +1,95 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-  <v-layout row>
-    <v-flex xs12 sm12 md6 offset-md3>
+    <v-layout row>
+        <v-flex xs12 sm12 md6 offset-md3>
 
-      <!--Add food dialog-->
-      <add-food-dialog :addFoodDialog="addFoodDialog" @close="addFoodDialog=false"/>
+            <!--Add food dialog-->
+            <add-food-dialog :addFoodDialog="addFoodDialog" @close="addFoodDialog=false"/>
 
-      <v-card>
-        <v-toolbar color="grey lighten-3" class="elevation-0">
+            <v-card>
+                <v-toolbar color="grey lighten-3" class="elevation-0">
 
-          <v-toolbar-title>List of my food</v-toolbar-title>
+                    <v-toolbar-title>List of my food</v-toolbar-title>
 
-          <v-spacer></v-spacer>
+                    <v-spacer></v-spacer>
 
 
-          <v-btn icon
-                 @click.stop="addFoodDialog = true"
-          >
-            <v-icon>add</v-icon>
-          </v-btn>
-          <!--          <v-btn icon>-->
-          <!--            <v-icon>search</v-icon>-->
-          <!--          </v-btn>-->
-        </v-toolbar>
+                    <v-btn icon
+                           @click.stop="addFoodDialog = true"
+                    >
+                        <v-icon>add</v-icon>
+                    </v-btn>
+                    <!--          <v-btn icon>-->
+                    <!--            <v-icon>search</v-icon>-->
+                    <!--          </v-btn>-->
+                </v-toolbar>
 
-        <v-list two-line class="pa-0">
-          <template v-for="item in foods">
+                <v-list two-line class="pa-0">
+                    <template v-for="item in foods">
 
-            <v-divider/>
+                        <v-divider/>
 
-            <v-list-tile
-                    :key="item.id"
-                    avatar
-                    @click=""
-            >
-              <v-list-tile-avatar>
-                <img src="https://cdn.vuetifyjs.com/images/lists/1.jpg">
-              </v-list-tile-avatar>
+                        <v-list-tile
+                                :key="item.id"
+                                avatar
+                                @click=""
+                        >
+                            <v-list-tile-avatar>
+                                <v-icon color="grey lighten-1" large>fastfood</v-icon>
+                            </v-list-tile-avatar>
 
-              <v-list-tile-content>
-                <v-list-tile-title v-html="item.name"></v-list-tile-title>
-                <v-list-tile-sub-title>
-                  100 gr of product contains -
-                  proteins {{item.proteins}} gr,
-                  fat {{item.fat}} gr,
-                  carbohydrates {{item.carbohydrates}} gr,
-                  calories {{item.calories}} gr.
-                </v-list-tile-sub-title>
-              </v-list-tile-content>
+                            <v-list-tile-content>
+                                <v-list-tile-title v-html="item.name"></v-list-tile-title>
+                                <v-list-tile-sub-title>
+                                    100 gr of product contains -
+                                    proteins {{item.proteins}}gr,
+                                    fat {{item.fat}}gr,
+                                    carbohydrates {{item.carbohydrates}}gr,
+                                    calories {{item.calories}}gr.
+                                </v-list-tile-sub-title>
+                            </v-list-tile-content>
 
-              <v-list-tile-action>
-                <v-btn icon ripple
-                       @click="deleteFood(item.id)"
-                >
-                  <v-icon color="grey lighten-1">delete</v-icon>
-                </v-btn>
-              </v-list-tile-action>
-            </v-list-tile>
-          </template>
-        </v-list>
-      </v-card>
-    </v-flex>
-  </v-layout>
+                            <v-list-tile-action>
+                                <v-btn icon ripple
+                                       @click="deleteFood(item.id)"
+                                >
+                                    <v-icon color="grey lighten-1">delete</v-icon>
+                                </v-btn>
+                            </v-list-tile-action>
+                        </v-list-tile>
+                    </template>
+                </v-list>
+            </v-card>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
-  import AddFoodDialog from 'components/ListOfFood/AddFoodDialog.vue'
-  import {mapState} from 'vuex'
-  import {mapActions} from 'vuex'
+    import AddFoodDialog from 'components/ListOfFood/AddFoodDialog.vue'
+    import {mapState} from 'vuex'
+    import {mapActions} from 'vuex'
 
-  export default {
-    components: {
-      AddFoodDialog,
-    },
-    computed: mapState(
+    export default {
+        components: {
+            AddFoodDialog,
+        },
+        computed: mapState(
             ['foods']
-    ),
-    data () {
-      return {
-        addFoodDialog : false,
-      }
-    },
-    methods: {
-      ...mapActions(['deleteFoodAction']),
-      deleteFood(id){
-        //Delete in server
-        this.$http.delete('/api/food/' + id)
-        //Delete in vuex
-        this.deleteFoodAction(id)
-      }
+        ),
+        data () {
+            return {
+                addFoodDialog : false,
+            }
+        },
+        methods: {
+            ...mapActions(['deleteFoodAction']),
+            deleteFood(id){
+                //Delete in server
+                this.$http.delete('/api/food/' + id)
+                //Delete in vuex
+                this.deleteFoodAction(id)
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
--- src/main/resources/asset/js/router.js
+++ src/main/resources/asset/js/router.js
@@ -5,11 +5,13 @@ Vue.use(VueRouter)
 import Welcome from './components/Welcome.vue'
 import ListOfFood from './components/ListOfFood/ListOfFood.vue'
 import AddFoodDialog from './components/ListOfFood/AddFoodDialog.vue'
+import ListOfEatenFood from './components/ListOfEatenFood/ListOfEatenFood.vue'
 
 const routes = [
     { path: '/welcome', component: Welcome },
     { path: '/ListOfFood', component: ListOfFood },
     { path: '/AddFood', component: AddFoodDialog },
+    { path: '/ListOfEatenFood', component: ListOfEatenFood },
 ]
 
 export const router = new VueRouter({
