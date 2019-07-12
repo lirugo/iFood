8a7b513aced59b44f3e02d61a3b94fe3fdea991d
lirugo <roostik21@gmail.com>
2019-07-12 12:25:59 +0300

    Added spring data rest webmvc

--- src/main/java/ua/iFood/config/AppConfig.java
+++ src/main/java/ua/iFood/config/AppConfig.java
@@ -2,8 +2,11 @@ package ua.iFood.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @Configuration
 @ComponentScan("ua.iFood.*")
+@Import(RepositoryRestMvcConfiguration.class)
 public class AppConfig {
 }
---
+++ src/main/java/ua/iFood/config/RestConfig.java
@@ -0,0 +1,16 @@
+package ua.iFood.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import ua.iFood.entity.Food;
+
+@Configuration
+class RestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Food.class);
+        config.setBasePath("/api");
+    }
+}
