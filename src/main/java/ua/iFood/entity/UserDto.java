7862f5cb72dc2f5498d9474a0bc2f21164ffcb2e
lirugo <roostik21@gmail.com>
2019-08-13 15:36:34 +0300

    Register and login logic - dto object

---
+++ src/main/java/ua/iFood/entity/UserDto.java
@@ -0,0 +1,20 @@
+package ua.iFood.entity;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+@Data
+public class UserDto {
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private String password;
+    private String matchingPassword;
+
+    @NotNull
+    private String email;
+}
